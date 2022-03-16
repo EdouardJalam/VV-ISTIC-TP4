d1fb0a0a7f635716975580dcadc7ac8e96ad9d45
EdouardJalam <100966437+EdouardJalam@users.noreply.github.com>
2022-03-16 15:03:41 +0100

    Update RomanNumeralTest.java

--- code/roman-numerals/src/test/java/fr/istic/vv/RomanNumeralTest.java
+++ code/roman-numerals/src/test/java/fr/istic/vv/RomanNumeralTest.java
@@ -3,7 +3,32 @@ import net.jqwik.api.*;
 
 public class RomanNumeralTest {
     @Property
-    boolean absoluteValueOfAllNumbersIsPositive(@ForAll int anInteger) {
-        return Math.abs(anInteger) >= 0;
+    boolean integerToRomanNumeral(@ForAll("integers") int integers) {
+    	String roman = RomanNumeraUtils.toRomanNumeral(integers);
+    	if(RomanNumeraUtils.isValidRomanNumeral(roman)) {
+    		return RomanNumeraUtils.parseRomanNumeral(roman) == integers;
+    	}
+    	return false;
     }
+    
+    @Property
+    boolean romanNumeralToInteger(@ForAll("RomanNumeral") String roman) {
+    	if(RomanNumeraUtils.isValidRomanNumeral(roman)) {
+    		int integers = RomanNumeraUtils.parseRomanNumeral(roman);
+    		String res = RomanNumeraUtils.toRomanNumeral(integers);
+        	return res.equals(roman) && RomanNumeraUtils.isValidRomanNumeral(res);
+    	}
+    	return true;
+    	
+    }
+    
+    @Provide
+	Arbitrary<Integer> integers() {
+		return Arbitraries.integers().between(1, 3999);
+	}
+    
+    @Provide
+	Arbitrary<String> RomanNumeral() {
+    	return Arbitraries.strings().withChars(new char[] {'M','D','C','L','X','V','I'}).ofMaxLength(9);
+	}
 }
