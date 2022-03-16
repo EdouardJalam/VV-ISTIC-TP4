ef54ed0a8c490dd82aa8b8231405740b685a1cfe
EdouardJalam <100966437+EdouardJalam@users.noreply.github.com>
2022-03-16 15:07:31 +0100

    Update BinaryHeapTest.java

--- code/heap/src/test/java/fr/istic/vv/BinaryHeapTest.java
+++ code/heap/src/test/java/fr/istic/vv/BinaryHeapTest.java
@@ -1,10 +1,25 @@
 package fr.istic.vv;
 import net.jqwik.api.*;
-
+import java.util.Comparator;
 
 public class BinaryHeapTest {
     @Property
-    boolean absoluteValueOfAllNumbersIsPositive(@ForAll int anInteger) {
-        return Math.abs(anInteger) >= 0;
+    <T> boolean heapTest(@ForAll("multipleBinaryHeap") BinaryHeap<T> heap) {
+    	T last = heap.pop();
+    	while(heap.count() > 0) {
+        	if(heap.getComparator().compare(last, last=heap.pop()) > 0) return false;
+        }
+    	return true;
     }
+    
+    @Provide
+	Arbitrary<BinaryHeap<Integer>> multipleBinaryHeap() {
+    	Comparator<Integer> cInt = (Integer a,Integer b) -> Integer.compare(a, b);
+    	Arbitrary<Integer> ints = Arbitraries.integers().between(0, 100);
+		return Arbitraries.integers().between(0, 10).map(i -> {
+			BinaryHeap<Integer> heap = new BinaryHeap<Integer>(cInt);
+			ints.forEachValue(val -> heap.push(val));
+			return heap;
+			});
+	}
 }
