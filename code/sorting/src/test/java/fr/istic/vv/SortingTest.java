dde5f026f3fc6b09a9d908cf65f20d8a21e63aaa
EdouardJalam <100966437+EdouardJalam@users.noreply.github.com>
2022-03-16 15:05:42 +0100

    Update SortingTest.java

--- code/sorting/src/test/java/fr/istic/vv/SortingTest.java
+++ code/sorting/src/test/java/fr/istic/vv/SortingTest.java
@@ -1,9 +1,41 @@
 package fr.istic.vv;
 import net.jqwik.api.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 
 public class SortingTest {
     @Property
-    boolean absoluteValueOfAllNumbersIsPositive(@ForAll int anInteger) {
-        return Math.abs(anInteger) >= 0;
+    boolean bubblesortTestInteger(@ForAll("arrayInteger") Integer[] tab) {
+    	Comparator<Integer> c = (Integer a,Integer b) -> Integer.compare(a, b);
+    	Integer[] t = Sorting.bubblesort(tab, c);
+        List<Integer> l = Arrays.asList(tab);
+        l.sort(c);
+        return Arrays.equals(t,l.toArray(new Integer[0]));
+    }
+    
+    @Property
+    boolean quicksortTestInteger(@ForAll("arrayInteger") Integer[] tab) {
+    	Comparator<Integer> c = (Integer a,Integer b) -> Integer.compare(a, b);
+    	Integer[] t = Sorting.quicksort(tab, c);
+        List<Integer> l = Arrays.asList(tab);
+        l.sort(c);
+        return Arrays.equals(t,l.toArray(new Integer[0]));
+    }
+    
+    @Property
+    boolean mergesortTestInteger(@ForAll("arrayInteger") Integer[] tab) {
+    	Comparator<Integer> c = (Integer a,Integer b) -> Integer.compare(a, b);
+    	Integer[] t = Sorting.mergesort(tab, c);
+        List<Integer> l = Arrays.asList(tab);
+        l.sort(c);
+        return Arrays.equals(t,l.toArray(new Integer[0]));
+    }
+    
+    @Provide
+    Arbitrary<Integer[]> arrayInteger() {
+    	Arbitrary<Integer> integerArbitrary = Arbitraries.integers().between(1, 100);
+    	Arbitrary<Integer[]> arrayArbitrary = integerArbitrary.array(Integer[].class).ofMinSize(0).ofMaxSize(10);
+    	return arrayArbitrary;
     }
 }
